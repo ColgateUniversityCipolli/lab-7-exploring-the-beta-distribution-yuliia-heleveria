620f54b08c397665a11cf242873eb8dcba89e1d7
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-03-04 15:01:00 -0500

    Finished task three

--- Lab07.R
+++ Lab07.R
@@ -1,5 +1,6 @@
 #load libraries
 library(tidyverse)
+library(e1071)
 
 #Task 1 - describe the population distribution
 
@@ -135,3 +136,149 @@ stats.fourth.tibble <- tibble(mean = mean.fourth,
                              skewness = skew.fourth,
                              excess_kurtosis = kurt.fourth)
 
+#Task 2: compute the moments
+#function that computes centered and uncentered moments
+beta.moment <- function(alpha, beta, k, centered){
+  moment <- 0
+  if (centered == F){ #uncentered moments
+    moment <- integrate(function(x){x^k*dnorm(x, mean=alpha, sd=beta)},
+              lower = -Inf, upper = Inf)
+  }else{ #centered moments
+    moment <- integrate(function(x){(x- dnorm(x, mean=alpha, sd=beta))^k},
+                        lower = -Inf, upper = Inf)
+  }
+  return(moment)
+}
+
+#confirm the function is working by computing population-level characteristics
+beta.moment(2, 5, 20, T)
+beta.moment(2, 5, 20, F)
+beta.moment(5, 5, 1000, F)
+
+#Task 3
+set.seed(7272) #set seed
+
+#first distribution 
+sample.size <- 500 # Specify sample details
+beta.first.sample <- rbeta(n = sample.size,  # sample size
+                     shape1 = alpha.first,   # alpha parameter
+                     shape2 = beta.first)    # beta parameter
+#plot the histogram for distribution
+beta.first.hist <- ggplot()+
+  geom_histogram(data = tibble(beta.first.sample), aes(x = beta.first.sample, y=after_stat(density)),
+                 color = "black", fill = "lightgray")+ #plot histogram
+  #plot sample density
+  geom_density(data = tibble(beta.first.sample), aes(x = beta.first.sample, y=after_stat(density), color="BetaSample(2,5)"))+
+  #plot actual Beta density
+  geom_line(data = first.dist, aes(x = x, y=beta.pdf, color="Beta(2,5)"))+
+  theme_bw()+
+  labs(
+    title = "Histogram and Estimated Density of Beta Sample",
+    x = "Sample of Beta Distribution",
+    y = "Density"
+  )+
+  scale_color_manual( #complete the legend
+    name = "Density",
+    values = c("BetaSample(2,5)" = "red", "Beta(2,5)" = "blue"),
+    labels = c("Beta Population", "Beta Sample"))+
+  theme(legend.position = "bottom")
+
+#include numerical summaries
+beta.first.summary <- tibble(beta.first.sample) %>%
+  summarize(mean = mean(beta.first.sample),
+            variance = var(beta.first.sample),
+            skewness = skewness(beta.first.sample),
+            excess_kurtosis = kurtosis(beta.first.sample))
+
+#second distribution
+beta.second.sample <- rbeta(n = sample.size,  # sample size
+                           shape1 = alpha.second,   # alpha parameter
+                           shape2 = beta.second)    # beta parameter
+#plot the histogram for distribution
+beta.second.hist <- ggplot()+
+  geom_histogram(data = tibble(beta.second.sample), aes(x = beta.second.sample, y=after_stat(density)),
+                 color = "black", fill = "lightgray")+ #plot histogram
+  #plot sample density
+  geom_density(data = tibble(beta.second.sample), aes(x = beta.second.sample, y=after_stat(density), color="BetaSample(5,5)"))+
+  #plot actual Beta density
+  geom_line(data = second.dist, aes(x = x, y=beta.pdf, color="Beta(5,5)"))+
+  theme_bw()+
+  labs(
+    title = "Histogram and Estimated Density of Beta Sample",
+    x = "Sample of Beta Distribution",
+    y = "Density"
+  )+
+  scale_color_manual( #complete the legend
+    name = "Density",
+    values = c("BetaSample(5,5)" = "red", "Beta(5,5)" = "blue"),
+    labels = c("Beta Population", "Beta Sample"))+
+  theme(legend.position = "bottom")
+
+#include numerical summaries
+beta.second.summary <- tibble(beta.second.sample) %>%
+  summarize(mean = mean(beta.second.sample),
+            variance = var(beta.second.sample),
+            skewness = skewness(beta.second.sample),
+            excess_kurtosis = kurtosis(beta.second.sample))
+
+#third distribution
+beta.third.sample <- rbeta(n = sample.size,  # sample size
+                            shape1 = alpha.third,   # alpha parameter
+                            shape2 = beta.third)    # beta parameter
+#plot the histogram for distribution
+beta.third.hist <- ggplot()+
+  geom_histogram(data = tibble(beta.third.sample), aes(x = beta.third.sample, y=after_stat(density)),
+                 color = "black", fill = "lightgray")+ #plot histogram
+  #plot sample density
+  geom_density(data = tibble(beta.third.sample), aes(x = beta.third.sample, y=after_stat(density), color="BetaSample(5,2)"))+
+  #plot actual Beta density
+  geom_line(data = third.dist, aes(x = x, y=beta.pdf, color="Beta(5,2)"))+
+  theme_bw()+
+  labs(
+    title = "Histogram and Estimated Density of Beta Sample",
+    x = "Sample of Beta Distribution",
+    y = "Density"
+  )+
+  scale_color_manual( #complete the legend
+    name = "Density",
+    values = c("BetaSample(5,2)" = "red", "Beta(5,2)" = "blue"),
+    labels = c("Beta Population", "Beta Sample"))+
+  theme(legend.position = "bottom")
+
+#include numerical summaries
+beta.third.summary <- tibble(beta.third.sample) %>%
+  summarize(mean = mean(beta.third.sample),
+            variance = var(beta.third.sample),
+            skewness = skewness(beta.third.sample),
+            excess_kurtosis = kurtosis(beta.third.sample))
+
+#fourth distribution
+beta.fourth.sample <- rbeta(n = sample.size,  # sample size
+                            shape1 = alpha.fourth,   # alpha parameter
+                            shape2 = beta.fourth)    # beta parameter
+#plot the histogram for distribution
+beta.fourth.hist <- ggplot()+
+  geom_histogram(data = tibble(beta.fourth.sample), aes(x = beta.fourth.sample, y=after_stat(density)),
+                 color = "black", fill = "lightgray")+ #plot histogram
+  #plot sample density
+  geom_density(data = tibble(beta.fourth.sample), aes(x = beta.fourth.sample, y=after_stat(density), color="BetaSample(0.50,0.50)"))+
+  #plot actual Beta density
+  geom_line(data = fourth.dist, aes(x = x, y=beta.pdf, color="Beta(0.50,0.50)"))+
+  theme_bw()+
+  labs(
+    title = "Histogram and Estimated Density of Beta Sample",
+    x = "Sample of Beta Distribution",
+    y = "Density"
+  )+
+  scale_color_manual( #complete the legend
+    name = "Density",
+    values = c("BetaSample(0.50,0.50)" = "red", "Beta(0.50,0.50)" = "blue"),
+    labels = c("Beta Population", "Beta Sample"))+
+  theme(legend.position = "bottom")
+
+#include numerical summaries
+beta.fourth.summary <- tibble(beta.fourth.sample) %>%
+  summarize(mean = mean(beta.fourth.sample),
+            variance = var(beta.fourth.sample),
+            skewness = skewness(beta.fourth.sample),
+            excess_kurtosis = kurtosis(beta.fourth.sample))
